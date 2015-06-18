6b5c085249479d6c2b310245306a27a10d0d12c2
unknown <jsomiset@JSOMISET-IN.oradev.oraclecorp.com>
2015-06-18 22:00:49 +0530

    Refactoring

--- src/test/java/com/jsomiset/common/RestBase.java
+++ src/test/java/com/jsomiset/common/RestBase.java
@@ -2,31 +2,32 @@ package com.jsomiset.common;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Form;
-import javax.ws.rs.core.MediaType;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.junit.After;
 
 public class RestBase {
 	public static String BASE_URI;
 	public static String USER_NAME;
 	public static String PASSWORD;
+	public static String GRIZZLY_PORT;
 	public Client client;
 	public WebTarget target;
 	private HttpServer server;
 
 	static {
-		BASE_URI = "http://localhost:8081/TestApp";
+		// TODO Should be loaded from config properties
+		GRIZZLY_PORT = "8081";
+		BASE_URI = String.format("http://localhost:%s/TestApp", GRIZZLY_PORT);
 	}
 
 	public RestBase() {
 		this.server = Server.startServer();
 		this.client = ClientBuilder.newClient();// Pass config object TODO
-		// authenticate(this.client);
+		authenticate(this.client);
 	}
 
 	/**
@@ -36,27 +37,30 @@ public class RestBase {
 	 */
 	public void setResourcePath(String path) {
 		this.target = this.client.target(BASE_URI).path(path);
-		// ADD common Headers
+		// TODO ADD common Headers
 	}
 
 	public void authenticate(Client client) {
 		// Build the target
-		Form form = new Form();
-		form.param("x", "foo");
-		form.param("y", "bar");
-
-		target.request(MediaType.APPLICATION_JSON_TYPE)
-				.post(Entity.entity(form,
-						MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+//		Form form = new Form();
+//		form.param("x", "foo");
+//		form.param("y", "bar");
+//
+//		target.request(MediaType.APPLICATION_JSON_TYPE)
+//				.post(Entity.entity(form,
+//						MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+		HttpAuthenticationFeature feature = HttpAuthenticationFeature.basic("admin", "admin");
+		client.register(feature);
 	}
 
 	/**
 	 * Common Configurations
 	 * 
-	 * @return
+	 * @return ClientConfig
 	 */
 	public ClientConfig configure() {
 		ClientConfig clientConfig = new ClientConfig();
+		// Register Filters & etc..
 		// clientConfig.register(MyClientResponseFilter.class);
 		// clientConfig.register(new AnotherClientFilter());
 		return clientConfig;
@@ -65,7 +69,7 @@ public class RestBase {
 	@After
 	public void close() {
 		this.client.close();
-		this.server.stop();
+		this.server.shutdown();
 	}
 
 }
--- src/test/java/com/jsomiset/resources/BookResourceTest.java
+++ src/test/java/com/jsomiset/resources/BookResourceTest.java
@@ -3,67 +3,19 @@ package com.jsomiset.resources;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jsomiset.common.Server;
+import com.jsomiset.common.RestBase;
 import com.jsomiset.resourcesImpl.BookResourceImpl;
 
-public class BookResourceTest {
-
-	private static HttpServer server;
-	private WebTarget resourceTarget;
-
+public class BookResourceTest extends RestBase{
 	public BookResourceTest() {
-		System.out.println("constructor is called"); // TODO Initialize
+		this.setResourcePath(BookResourceImpl.ITEMS_URL);
 	}
-
-	@BeforeClass
-	public static void beforeBookApiTestClass() {
-		server = new Server().startServer();
-		System.out.println("Server was started");
-	}
-
-	@Before
-	public void beforeBookApiTest() throws Exception {
-		Client client = ClientBuilder.newClient();// http://localhost:8081/TestApp/api
-		HttpAuthenticationFeature feature = HttpAuthenticationFeature.basic("admin", "admin");
-		client.register(feature);
-		WebTarget target = client.target("http://localhost:8081/TestApp");// /api/v1/books
-		resourceTarget = target.path(BookResourceImpl.ITEMS_URL);
-		ObjectMapper mapper = new ObjectMapper();
-	}
-
+	
 	@Test
 	public void v1ItemsShouldReturnStatus200() {
-		assertThat(resourceTarget.request().head().getStatus(), is(200));
+		assertThat(this.target.request().head().getStatus(), is(200));
 	}
-
-	// @Test
-	// public void testForGetAllBooks() {
-	// Invocation.Builder invocationBuilder =
-	// this.target.request(MediaType.APPLICATION_JSON);
-	// Response response = invocationBuilder.get();
-	// System.out.println("Status :" + response.getStatus());
-	// System.out.println("Response:" + response.readEntity(List.class));
-	// // List<Object> books = response.readEntity(List.class);
-	// // for(Object book: books){
-	// // System.out.println("Each book is : "+ (Book)book);
-	// // }
-	// }
-
-	@AfterClass
-	public static void afterUserResourceTestClass() {
-		System.out.println("server is going to shut down");
-		server.shutdown();
-	}
+	
 }
